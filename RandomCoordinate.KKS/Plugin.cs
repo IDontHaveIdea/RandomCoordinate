c463ad19b65720f4839bd598d116495af04e86a8
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-10-31 15:32:25 -0400

    Add dependency in IDHIUtils

--- RandomCoordinate.KKS/Plugin.cs
+++ RandomCoordinate.KKS/Plugin.cs
@@ -8,6 +8,7 @@ using KKAPI;
 namespace IDHIPlugins
 {
     [BepInDependency(KoikatuAPI.GUID, KoikatuAPI.VersionConst)]
+    [BepInDependency("com.ihavenoidea.idhiutils", "1.0.1")]
     [BepInPlugin(GUID, PluginDisplayName, Version)]
     [BepInProcess(KoikatuAPI.GameProcessName)]
     [BepInProcess(KoikatuAPI.VRProcessName)]
