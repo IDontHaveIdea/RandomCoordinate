717773f281a9950cd81126f2913f8d32b089e72a
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2023-01-31 11:41:03 -0500

    Update to IDHIPluginInfo

--- RandomCoordinate.KKS/Plugin.cs
+++ RandomCoordinate.KKS/Plugin.cs
@@ -9,7 +9,7 @@ using KKAPI;
 namespace IDHIPlugins
 {
     [BepInDependency(KoikatuAPI.GUID, KoikatuAPI.VersionConst)]
-    [BepInDependency(IDHIUtils.Utilities.GUID, IDHIUtils.Utilities.Version)]
+    [BepInDependency(IDHIUtils.IDHIUtilsInfo.GUID, IDHIUtils.IDHIUtilsInfo.Version)]
     [BepInPlugin(GUID, PluginDisplayName, Version)]
     [BepInProcess(KoikatuAPI.GameProcessName)]
     public partial class RandomCoordinatePlugin : BaseUnityPlugin
