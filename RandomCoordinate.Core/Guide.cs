47932500be5239e8b398adaab25989b67e1f75af
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2023-03-05 23:29:27 -0500

    Fix correctly detect map number where the Guide is in

--- RandomCoordinate.Core/Guide.cs
+++ RandomCoordinate.Core/Guide.cs
@@ -12,6 +12,7 @@ namespace IDHIPlugins
     {
         internal static SaveData.Heroine _guide;
         internal static int _guideMapNo;
+        internal static ActionGame.Chara.Fix _fixChara;
 
         /// <summary>
         /// Save Heroine information for the Guide Character
@@ -22,45 +23,50 @@ namespace IDHIPlugins
             SaveData.Heroine heroine,
             bool setCoordinate = false)
         {
+            if (heroine.fixCharaID != -13)
+            {
+                return;
+            }
             var hashes = new StringBuilder();
             var guideMap = -1;
-            if (Manager.Game.saveData.guideSetPositionMaps != null)
+            var fixChara = heroine.charaBase as ActionGame.Chara.Fix;
+            var mapMove = -1;
+            var mapFix = -1;
+            var ctrl = GetController(heroine.chaCtrl);
+            if (fixChara != null)
             {
-                hashes.Append("[ ");
-                foreach (var h in Manager.Game.saveData.guideSetPositionMaps)
+                _fixChara = fixChara;
+                mapMove = fixChara.charaData.moveData.mapNo;
+                mapFix = fixChara.mapNo;
+                guideMap = mapMove;
+                if ((mapMove <= 0) && (mapFix != 0))
                 {
-                    if (Manager.Game.saveData.guideSetPositionMaps.Count == 1)
-                    {
-                        guideMap = h;
-                    }
-                    hashes.Append($"{h} ");
+                    guideMap = mapFix;
                 }
-                hashes.Append(']');
             }
-
             _guide = heroine;
             _guideMapNo = guideMap;
-#if DEBUG
-            _Log.Info($"[SetGuide] GUIDE={_guide.Name.Trim()} chaName={_guide.chaCtrl.name} " +
-                $"position maps={hashes} mapNo={guideMap}");
-#endif
             if (setCoordinate)
             {
                 // For the guide 
                 if (guideMap == 4)
                 {
-                    heroine.chaCtrl.fileStatus.coordinateType = (int)CoordinateType.Swim;
-                    ChangeCoordinate(heroine.chaCtrl, (int)CoordinateType.Swim);
+                    heroine.chaCtrl.fileStatus.coordinateType = (int)ChaFileDefine.CoordinateType.Swim;
+                    ctrl.SetRandomCoordinate(ChaFileDefine.CoordinateType.Swim);
+                    ChangeCoordinate(heroine.chaCtrl, (int)ChaFileDefine.CoordinateType.Swim);
                 }
                 else
                 {
                     // Guide won't be in any map that have special consideration
-                    var ctrl = GetController(heroine.chaCtrl);
                     var newCoordinate = ctrl.NewRandomCoordinateByType(
                                 ChaFileDefine.CoordinateType.Plain);
                     ChangeCoordinate(heroine.chaCtrl, newCoordinate);
                 }
             }
+#if DEBUG
+            _Log.Info($"[SetGuide] GUIDE={_guide.Name.Trim()} chaName={_guide.chaCtrl.name} " +
+                $"position guideMap={guideMap} mapMove={mapMove} mapFix={mapFix}.");
+#endif
         }
     }
 }
