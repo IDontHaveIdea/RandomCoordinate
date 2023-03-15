6ae61615577d316f41276651cf6578701f5ae20f
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2023-03-15 02:38:09 -0400

    Refactoring

--- RandomCoordinate.Core/Utilities.cs
+++ RandomCoordinate.Core/Utilities.cs
@@ -13,6 +13,31 @@ namespace IDHIPlugins
     public partial class Utilities
     {
         /// <summary>
+        /// Overload GirlNme(ChaControl)
+        /// </summary>
+        /// <param name="girl"></param>
+        /// <returns></returns>
+        public static string GirlName(SaveData.Heroine girl)
+        {
+            var name = "";
+
+            if (girl != null)
+            {
+                name = girl.Name.Trim();
+
+                if (name == null)
+                {
+                    if (GirlsNames.TryGetValue(girl.chaCtrl.name, out var rc))
+                    {
+                        name = rc;
+                    }
+                }
+            }
+
+            return name;
+        }
+
+        /// <summary>
         /// Get the girl name for debugging purposes. Sometimes looking in
         /// the Savedata.Heroine class fails.
         /// </summary>
@@ -20,33 +45,7 @@ namespace IDHIPlugins
         /// <returns></returns>
         public static string GirlName(ChaControl girl)
         {
-            var name = girl.GetHeroine()?.Name.Trim();
-
-            if (name == null)
-            {
-                _ = GirlsNames.TryGetValue(girl.name, out var rc);
-                name = rc;
-            }
-
-            return name;
-        }
-
-        /// <summary>
-        /// Overload GirlNme(ChaControl)
-        /// </summary>
-        /// <param name="girl"></param>
-        /// <returns></returns>
-        public static string GirlName(SaveData.Heroine girl)
-        {
-            var name = girl.Name.Trim();
-
-            if (name == null)
-            {
-                _ = GirlsNames.TryGetValue(girl.chaCtrl.name, out var rc);
-                name = rc;
-            }
-
-            return name;
+            return GirlName(girl.GetHeroine());
         }
 
         /// <summary>
