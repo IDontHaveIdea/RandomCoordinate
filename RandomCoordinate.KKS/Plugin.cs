85c5cec0648fc716db1a7e31e50d4eb47620f05a
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2024-08-19 12:12:56 -0400

    Update dependency version

--- RandomCoordinate.KKS/Plugin.cs
+++ RandomCoordinate.KKS/Plugin.cs
@@ -10,7 +10,7 @@ using KKAPI;
 namespace IDHIPlugins
 {
     [BepInDependency(KoikatuAPI.GUID, KoikatuAPI.VersionConst)]
-    [BepInDependency(IDHIUtils.IDHIUtilsInfo.GUID, "1.0.8.0")]
+    [BepInDependency(IDHIUtils.IDHIUtilsInfo.GUID, "1.1.0.0")]
     [BepInDependency("com.deathweasel.bepinex.moreoutfits",
         BepInDependency.DependencyFlags.SoftDependency)]
     [BepInPlugin(GUID, PluginDisplayName, Version)]
